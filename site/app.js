e59e92770e5c04e77d1efe2417be2cc830d26c8d
guillermokatz <guillokatz@gmail.com>
2020-09-15 17:02:19 -0300

    borro linea const dirname del app js

--- site/app.js
+++ site/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { dirname } = require('path');
+
 const app = express();
-app.use(express.static('public'))
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
