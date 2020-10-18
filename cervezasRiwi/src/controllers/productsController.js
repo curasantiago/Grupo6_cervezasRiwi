42014a217f511bb8e5badce9c459cf0be234061d
pachi <analia@MacBook-Air-de-analia.local>
2020-10-18 18:30:21 -0300

    comente lo que estoy haciendo

---
+++ cervezasRiwi/src/controllers/productsController.js
@@ -0,0 +1,43 @@
+//const controller={
+//   show:(req, res)=>{
+//      res.render("index");
+
+//  },   
+      
+//    login:(req, res)=>{
+//        res.render("login");
+//    }, 
+    
+//    ingresar:(req, res)=>{
+//        res.render("ingreso");
+//    },
+    
+//    pagar:(req, res)=>{
+//        res.render("productsCart");
+//    },
+    
+//    detalle:(req, res)=>{
+//        res.render("productsCart");
+//    },
+    
+//    carrito:(req, res)=>{
+//        res.render("ingreso");
+//    },
+//    registro:(req, res)=>{
+//        res.render("productsCart");
+//    },
+    
+//    crear:(req, res)=>{
+//        res.render("productCreateForm");
+//    }
+//    editar:(req, res)=>{
+//        res.render("productsEditForm");
+//    }
+//    buscar:(req, res)=>{
+//        res.render("productSearchForm");
+//    }
+//}
+
+
+
+module.export=controller;
--- cervezasRiwi/src/routes/index.js
+++ cervezasRiwi/src/routes/index.js
@@ -7,42 +7,44 @@ const validator = require('../middlewares/routes/validator');
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
+//router.get('/', ProductController.show); CP
 router.get('/login', (req, res) => {
   res.render('login', {title: "Login"})
 })
-
+//router.get('/login', userController.login); CP
 router.get('/ingreso', (req, res) => {
   res.render('ingreso')
 })
-
+//router.get('/ingreso', userController.ingresar); CP
 router.get('/payForm', (req, res) => {
   res.render('payForm')
 })
-
+//router.get('/payForm',productController.pagar); CP
 router.get('/productDetail', (req, res) => {
   res.render('productDetail');
 })
-
+//router.get('/productDetail', productController.detalle); CP
 router.get('/productCart', (req, res) => {
   res.render('productCart', {title: "Carrito de compras"});
 })
-
+//router.get('/productCart', productController.carrito); CP
 router.get('/productSearch', (req, res) => {
   res.render('productSearch');
 })
-
+//outer.get('/productSearch', productController.buscar); CP
 router.get('/registerForm', (req, res) => {
   res.render('registerForm');
 })
-
+//router.post('/registerForm', productController.registro); CP
 router.get('/productCreate', (req, res) => {
   res.render('productCreateForm');
 })
-
+//router.post('./Create', productController.crear); CP
 router.get('/productEdit', (req, res) => {
   res.render('productEditForm');
 })
+//outer.get('/productEdit', productController.editar); CP
+
 
 // router.post('/productCreate',
 // [check("name")
