b416bc1ca5d0737ea4ea5c50020f988a68cddd36
guillermokatz <guillokatz@gmail.com>
2020-10-20 16:52:15 -0300

    ejercitacion session incompleta

--- cervezasRiwi/src/controllers/userController.js
+++ cervezasRiwi/src/controllers/userController.js
@@ -1,4 +1,14 @@
+// const fs = require("fs");
+// const path = require("path");
+
+// const leerJSON = () => {
+//     let jsonUsers = fs.readFileSync(path.resolve(__dirname + "/../users.JSON"));
+//     return JSON.parse(jsonUsers);
+// }
+
 const userController={
+
+
     registro:(req, res)=>{
         res.render("users/registerForm", {title: "Registrarse"});
     },
@@ -9,8 +19,38 @@ const userController={
    
     pagar:(req, res)=>{
         res.render("users/payForm", {title: "Incluir medio de pago"});
-    },
+    }
+
 }
 
   module.exports = userController;
   
+
+      // processRegister: (req, res)=> {
+    //     let DBusuarios = leerJSON();
+        
+    //     let usuario = {
+    //         id: DBusuarios[DBusuarios.length -1].id + 1,
+    //         ...req.body
+    //     }
+
+    //     let nuevaDB = [...DBusuarios, usuario];
+    //     fs.writeFileSync(path.resolve(__dirname + "/../users.JSON"), JSON.stringify(nuevaDB, null, 2));
+    //     res.redirect("/login");
+    // },
+
+    // processLogin: (req, res)=> {
+    //     let DBusuarios = leerJSON();
+
+    //     let userEncontrado = "No encontrado";
+
+    //     DBusuarios.forEach(user => {
+    //         if(req.body.email == user.email) {
+    //             userEncontrado = user;
+    //         }
+    //     });
+
+    //     res.send(userEncontrado);
+        
+        
+    // }
--- cervezasRiwi/src/routes/users.js
+++ cervezasRiwi/src/routes/users.js
@@ -9,7 +9,11 @@ const userController = require('../controllers/userController');  //pachi
 
 //---------------vista-----------------------//
 router.get('/register', userController.registro); 
+// router.post('/register', userController.processRegister); 
+
 router.get('/login', userController.login); 
+// router.post('/login', userController.processLogin);
+
 router.get('/pay', userController.pagar);
 
 module.exports = router;
