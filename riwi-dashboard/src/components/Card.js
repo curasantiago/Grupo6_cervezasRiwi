de72ba7bbaa68aa8bfbf7ed1f4b896ffee2cdaa4
guillermokatz <guillokatz@gmail.com>
2021-01-19 17:48:06 -0300

    triple fetch a api products en card a reemplazar con otras apis

--- riwi-dashboard/src/components/Card.js
+++ riwi-dashboard/src/components/Card.js
@@ -1,7 +1,6 @@
 
-function Card (props) {
+function Card ({totalValue, totalName}) {
     
-    console.log(props.total)
     return (
         
         
@@ -12,7 +11,7 @@ function Card (props) {
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p className="card-text"><small className="text-muted">Total productos: {props.total}</small></p>
+              <p className="card-text"><small className="text-muted">Total {totalName}: {totalValue}</small></p>
             </div>
           </div>
         </div>
--- riwi-dashboard/src/components/TopRow.js
+++ riwi-dashboard/src/components/TopRow.js
@@ -7,30 +7,39 @@ class TopRow extends Component {
     constructor () {
         super();
         this.state= {
-            products : [],
-            total: []
+            totals: []
         }
     }
 
     componentDidMount () {
         console.log("Mounting!");
+
+        // FETCH 1
         fetch('/api/products/')
-        .then( response =>  {
-            return response.json() 
-        })
-        .then( data => {
-            this.setState({
-                productCount: data.metadata.count,
-                // total: [this.state.productCount]
-            })
-        })
+        .then( response =>  { return response.json() })
+        .then( data => { this.setState({ totals: [{idtotal: 'tot1', totalName:'Productos', value: data.metadata.count}] }) })
+        .catch(error => console.log(error));
+
+        // FETCH 2
+        fetch('/api/products/')
+        .then( response =>  { return response.json() })
+        .then( data => { this.setState({ totals: [...this.state.totals, {idtotal: 'tot2', totalName:'Usuarios', value: data.metadata.count}] }) })
+        .catch(error => console.log(error))
+
+        // FETCH 3
+        fetch('/api/products/')
+        .then( response =>  { return response.json() })
+        .then( data => { this.setState({ totals: [...this.state.totals, {idtotal: 'tot3', totalName:'Compras', value: data.metadata.count}] }) })
         .catch(error => console.log(error))
     }
 
     render () {
+    
         return (
     <section className="row">
-      {/* {this.state.total.map ( valor => {return <Card key={valor.id} total={valor.totalProductos}/>} )} */}
+        
+        {this.state.totals.map( cadaTotal => {return <Card key={cadaTotal.idtotal} totalName={cadaTotal.totalName} totalValue={cadaTotal.value}/>})}
+
       </section>
     )
     }
